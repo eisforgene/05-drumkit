635a49d188a95ed783a79a20a1ad7ccfcf0fc394
eisforgene <82473195+eisforgene@users.noreply.github.com>
2022-04-27 08:45:55 -0700

    added sound to keypress

---
+++ index.js
@@ -0,0 +1,51 @@
+// Check Button Press
+let drumButtons = document.querySelectorAll('.drum').length;
+
+for (let i = 0; i < drumButtons; i++) {
+    document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+        let buttonHTML = this.innerHTML;
+
+        makeSound(buttonHTML);
+    })
+}
+
+// Check Keyboard press
+document.addEventListener('keydown', function(e) {
+    makeSound(e.key);
+})
+
+
+let makeSound = function(key) {
+    switch (key) {
+        case 'a':
+            let snare = new Audio('./sounds/snare.mp3');
+            snare.play()
+            break;
+        case 's':
+            let tom2 = new Audio('./sounds/tom-2.mp3');
+            tom2.play()
+            break;
+        case 'd':
+            let tom3 = new Audio('./sounds/tom-3.mp3');
+            tom3.play()
+            break;
+        case 'f':
+            let tom4 = new Audio('./sounds/tom-4.mp3');
+            tom4.play()
+            break;
+        case 'j':
+            let tom1 = new Audio('./sounds/tom-1.mp3');
+            tom1.play()
+            break;
+        case 'k':
+            let bass = new Audio('./sounds/kick-bass.mp3');
+            bass.play()
+            break;
+        case 'l':
+            let crash = new Audio('./sounds/crash.mp3');
+            crash.play()
+            break;
+        default: 
+            console.log(key)
+    }
+}
